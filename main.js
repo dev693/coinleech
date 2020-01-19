feaf01eacd32aa5bd5a2a945c1d57e59cf099b12
Clemens Wagner <clemens@allvr.de>
2020-01-19 21:10:58 +0100

    added arguments, get history of candles

--- main.js
+++ main.js
@@ -2,37 +2,60 @@ const axios = require('axios');
 const fs = require('fs');
 
 const API = "https://api-pub.bitfinex.com/v2/";
-const INTERVAL = 10000;
-const LIMIT = 80;
-const symbol = "tETHUSD";
-const period = "1m"
+const INTERVAL = 6100;
+const DEFAULT_SYMBOL = "tETHUSD";
+const PERIOD = "1m"
 
 let lasttime = 0;
+let interval = null;
 
 main();
 
 
 async function main()
 {
-    lasttime = getTimestamp();
-    setInterval(async () => await getCandles(), INTERVAL)
+    console.log(process.argv);
+
+    let argv = process.argv;
+
+    if (argv.length < 3)
+        return;
+
+    let action = argv[2];
+    let symbol = argv.length >= 3 ? argv[3] : DEFAULT_SYMBOL;
+    lasttime = argv.length >= 4 ? parseInt(argv[4]) : 0;
+
+    switch (action.toLowerCase())
+    {
+        case "leech":
+            interval = setInterval(async () => await getCandles(symbol), INTERVAL)
+            break;
+    }
+
 }
 
-async function getCandles()
+async function getCandles(symbol)
 {
     try 
     {
-        const url = API + "candles/trade:" + period + ":" + symbol + "/hist?limit=" + LIMIT;
+        const url = API + "candles/trade:" + PERIOD + ":" + symbol + "/hist?start=" + lasttime + "&sort=1&limit=9999";
         const response = await axios.get(url);
         const candles = response.data;
-        for (let i = candles.length - 1; i >= 0 ; i--)
+        for (let i = 0; i < candles.length ; i++)
         {
             let time = candles[i][0]
-    
+            
+            if (isToday(time))
+            {
+                clearInterval(interval);
+                console.log("done!");
+                return;
+            }
+
             if (time <= lasttime)
                 continue;
     
-            saveOCHL(candles[i][0], candles[i][1], candles[i][2], candles[i][3], candles[i][4], candles[i][5]);
+            saveOCHL(symbol, candles[i][0], candles[i][1], candles[i][2], candles[i][3], candles[i][4], candles[i][5]);
         }
     }
     catch (e)
@@ -41,7 +64,16 @@ async function getCandles()
     }
 }
 
-async function saveOCHL(time, o, c, h, l, v)
+function isToday(timestamp)
+{
+    let today = new Date();
+    let time = new Date(timestamp);
+    time = time.setHours(0, 0, 0, 0);
+    today = today.setHours(0, 0, 0, 0);
+    return time == today;
+}
+
+async function saveOCHL(symbol, time, o, c, h, l, v)
 {
     if (time <= lasttime)
         return;
@@ -55,19 +87,18 @@ async function saveOCHL(time, o, c, h, l, v)
     let data = time + ";" + o + ";" + c + ";" + h + ";" + l + ";" + v;
     console.log(data);
     fs.appendFileSync(file, data + "\n");
-    saveTimestamp();
 }
 
-function getTimestamp()
-{   
-    let timestamp = symbol + "/timestamp";
-    if (!fs.existsSync(timestamp))
-        return 0;
-    return parseInt(fs.readFileSync(timestamp));
-}
+// function getTimestamp()
+// {   
+//     let timestamp = symbol + "/timestamp";
+//     if (!fs.existsSync(timestamp))
+//         return 0;
+//     return parseInt(fs.readFileSync(timestamp));
+// }
 
-function saveTimestamp()
-{
-    let timestamp = symbol + "/timestamp";
-    fs.writeFileSync(timestamp, lasttime);
-}
+// function saveTimestamp()
+// {
+//     let timestamp = symbol + "/timestamp";
+//     fs.writeFileSync(timestamp, lasttime);
+// }
